bed3c09d2b42d955b23d80422eaeca6d22a18658
yoosoonil <y2k7654@gmail.com>
2023-04-29 23:59:24 +0900

    Create serializer

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -15,8 +15,22 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework_jwt.views import obtain_jwt_token, verify_jwt_token, refresh_jwt_token
+from .views import validate_jwt_token
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    
+    path('validate/', validate_jwt_token),
+    path('login/', obtain_jwt_token),
+    
+    path('verify/', verify_jwt_token),
+    path('refresh/', refresh_jwt_token),
+    
+    path('user/', include('user.urls')),
 ]
+urlpatterns += \
+    static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- backend/user/views.py
+++ backend/user/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.contrib.auth.models import User
+from rest_framework import permissions, status, generics
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .serializers import UserSerializer, UserSerializerWithToken, ProfileSerializer
+from .models import Profile
 
 # Create your views here.
+@api_view(['GET'])
+def current_user(request):
+    serialzer = UserSerializer(request.user)
+    return Response(serialzer.data)
+
+class UserList(APIView):
+    permission_classes = (permissions.AllowAny,)
+    
+    def post(self, request, format=None):
+        serializer = UserSerializerWithToken(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+class ProfileUpdateAPI(generics.UpdateAPIView):
+    lookup_field = "user_pk"
+    queryset = Profile.objects.all()
+    serializer_class = ProfileSerializer
